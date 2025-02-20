5735c210646964d5c46896c545af9a8836b6a3b0
Amilton Bandeira <amiltonbraulioadaobandeira@gmail.com>
2025-02-20 16:02:30 +0100

    Update test.py
    figthing with # _*_ coding:  latin-1 _*_

--- Tests/test.py
+++ Tests/test.py
@@ -1,6 +1,8 @@
+#! usr/bin/env
+# _*_ coding: latin-1 _*_
 if True:
     print("Hello World")
     print("Hello World 2")
     print("Hello World again")
-    print("Hello World ++")
+    print("Hello World ++ 😊")
     print(True)
