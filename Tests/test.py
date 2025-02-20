375b69df846ecc235565c263bbaa1697977ec347
Amilton Bandeira <amiltonbraulioadaobandeira@gmail.com>
2025-02-20 16:09:18 +0100

    Update test.py

--- Tests/test.py
+++ Tests/test.py
@@ -1,8 +1,7 @@
-#! usr/bin/env
-# _*_ coding: latin-1 _*_
+
 if True:
     print("Hello World")
     print("Hello World 2")
     print("Hello World again")
-    print("Hello World ++ 😊")
+    print ("Hello World ++ 😊")
     print(True)
