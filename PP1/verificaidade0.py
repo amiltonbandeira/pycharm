387874509a2b0236c5b53001f3c0a5878f3909a4
Amilton Bandeira <amiltonbraulioadaobandeira@gmail.com>
2025-02-21 11:40:36 +0100

    create a new python file

---
+++ PP1/verificaidade0.py
@@ -0,0 +1,11 @@
+age = 12
+
+if age<18:
+    print("Voce e menor de idade")
+elif age == 18:
+    print("Voce acabou de atingir a maior idade")
+else:
+    print("Voce e maior de idade")
+    
+    
+print(age)
