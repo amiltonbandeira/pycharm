a0a7b6041e4e0d2cdf05427d1b6ac053c01eef0b
Amilton Bandeira <amiltonbraulioadaobandeira@gmail.com>
2025-02-21 10:29:07 +0100

    Update test.py
    added more sys.argv arguments

--- Tests/test.py
+++ Tests/test.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+# _*_ encoding: latin-1 _*_
 import sys
 
 is_flower = True
@@ -6,6 +8,13 @@ if is_flower :
   
 print("\n")
 print("Script name: " , sys.argv[0])
-print("Hello World: " , sys.argv[1])
+print("Hello World: " , sys.argv[1:])
+print("Hello World: " , sys.argv[2:])
+print("Hello World: " , sys.argv[3:])
+print("Hello World: " , sys.argv[4:])
+print("Hello World: " , sys.argv[5:])
+print("Hello World: " , sys.argv[6:])
+print("Hello World: " , sys.argv[7:])
+
 
 print("'hoe'")
