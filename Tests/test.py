4650e2dc62fd3d68c2edd10337dd6cadce0de8e8
Amilton Bandeira <amiltonbraulioadaobandeira@gmail.com>
2025-02-21 10:17:10 +0100

    alterned this thing , alterned the test.py archive

--- Tests/test.py
+++ Tests/test.py
@@ -1,7 +1,11 @@
+import sys
 
-if True:
-    print("Hello World")
-    print("Hello World 2")
-    print("Hello World again")
-    print ("Hello World ++ 😊")
-    print(True)
+is_flower = True
+if is_flower :
+    print("Beautiful hello world flower")
+  
+print("\n")
+print("Script name: " , sys.argv[0])
+print("Hello World: " , sys.argv[1])
+
+print("'hoe'")
